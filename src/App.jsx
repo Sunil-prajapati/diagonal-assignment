b92df0f2dcb3c5d4a9352333fb2306d147544a5a
sunilSupertal <sunil@supertal.io>
2024-10-28 17:55:32 +0530

    configured tailwind and font-family

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-red-500 font-regular'>Main App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/global/BackButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BackButton = () => {
+    return (
+        <div
+            className=""
+            onClick={() => {
+            }}
+        >
+            <img src="https://test.create.diagnal.com/images/Back.png" width='40' height='40' />
+        </div>
+    );
+};
+
+export default BackButton;
